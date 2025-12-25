c4a0855204a1ec34bc17aa46c74a3b9d935f8750
eevkyi <eevkyi@outlook.com>
2025-12-25 18:48:22 -0300

    docs: add plugin header

---
+++ EEV_TitleScreenMod.js
@@ -0,0 +1,24 @@
+/*:
+ * @target MV MZ
+ *
+ * @plugindesc Title Screen Modifications.
+ * [v0.0.0] [MIT License] [Compatible with MV and MZ]
+ *
+ * @author eevkyi and contributors
+ *
+ * @url https://ko-fi.com/eevkyi
+ *
+ * @help
+ * No settings available in the current version.
+ *
+ * ----------------------------------------------------------------------------
+ * TODO v0.1.0:
+ * - [Screen] Text with customizable size.
+ * - [Menu] Quit option with customizable text.
+ * - [Menu] External links with customizable text.
+ * - [Menu] Customizable width and visible rows.
+ *
+ * ----------------------------------------------------------------------------
+ * Releases:
+ * - https://github.com/eevkyi/EEV_TitleScreenMod.js
+ */
